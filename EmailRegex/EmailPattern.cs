fb6dd8ba0b0b6b4dac8a8fbae9d4273ef9f1090f
N1vas <ggj.nivas.18@gmail.com>
2020-10-06 11:20:33 +0530

    [N1vas] Add : UC1 Regex of email added

---
+++ EmailRegex/EmailPattern.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace EmailRegex
+{
+    class EmailPattern
+    {
+        string Regex_Email = "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*[@][0-9a-zA-Z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
+        public bool ValidateEmail(string pincode)
+        {
+            return Regex.IsMatch(pincode, Regex_Email);
+        }
+    }
+}
---
+++ EmailRegex/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace EmailRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmailPattern pattern = new EmailPattern();
+            Console.WriteLine("Enter email id to be checked with regex");
+            String mail = Console.ReadLine(); 
+            bool result = pattern.ValidateEmail(mail);
+            Console.WriteLine(result);
+        }
+    }
+}
